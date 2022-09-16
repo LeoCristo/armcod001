2351204ce26216463244b5730ee1ef55263b3844
leo braga <leonardobragadecristo@gmail.com>
2022-09-16 07:11:28 -0300

    Adicionado funcoes para captura de numero primo e alteracao na ordem do bubble sort, ordem crescente

--- exemplo.s
+++ exemplo.s
@@ -28,27 +28,59 @@
 
 ; -------------------------------------------------------------------------------
 INICIAL EQU 0x20000D00 ;endereco na ram
+PRIMO   EQU 0x20000E00 ;Números primos da lista
 ; Função main()
 Start  
 
 ; Comece o código aqui <======================================================
 	LDR R0,=TEXTO ;; Pega o endereco do primeiro byte
 	LDR R2,=INICIAL
-
-REPETIR
+;;Funcao para a lista com os números aleatórios na posição INICIAL
+REPETIR0
 	LDRB R1,[R0],#1;;armazena o dado de endereco r0 no registrador r1, incrementa r0 em 1
 	STRB R1,[R2],#1;;coloca o dado 
 	CMP R1,#0
-	BEQ FIM
-	B REPETIR
-FIM
+	BEQ FIM0
+	B REPETIR0
+FIM0
+
+;Funcao para a lista com os números primos na posição PRIMO
+	LDR R0,=INICIAL
+	LDR R2,=PRIMO
+REPETIR1
+	LDRB R1,[R0],#1;;armazena o dado de endereco r0 no registrador r1, incrementa r0 em 1
+	CMP R1,#0
+	BEQ FIM1
+;;Testa se o número é primo
+	MOV R6,R1
+	MOV R7,#1
+	MOV R8,#0
+TESTE
+	CMP R7,R6
+	BEQ ADICIONAR
+	ADD R7,#1
+	UDIV R5,R1,R7;;R2=R1/R7
+	MLS R3,R5,R7,R6;;R3=R6-R5*R7
+	CMP R3,#0
+	BNE TESTE
+	ADD R8,#1
+	B TESTE
+;;Fim do teste
+
+;;Adiciona o número na memória
+ADICIONAR
+	CMP R8,#1
+	BNE REPETIR1
+	STRB R1,[R2],#1;;coloca o dado 
+	B REPETIR1
+FIM1
 
 ;;Funcao de ordenacao
 
-	MOV R6,#4;;indice do loop 1
+	MOV R6,#19;;indice do loop 1
 	
 LOOP1
-	MOV R7,#4;;indice do loop 2
+	MOV R7,#19;;indice do loop 2
 	CMP R6,#0
 	BEQ FIM2;;TESTA INIDICE DO LOOP1
 	SUB R6,#1;;Decrementa o indice 1
@@ -64,7 +96,7 @@ LOOP3
 	LDRB R1,[R2,#1];;pega o dado seguinte
 	ADD R2,#1;;Incrementa o endereco base da ram
 	CMP R0,R1;;Compara r0 e r1
-	BHI LOOP3;;se r0 for maior que r1 repete o loop 3 sem trocar
+	BLO LOOP3;;se r0 for maior que r1 repete o loop 3 sem trocar
 	SUB R2,#1;;Decrementa o endereco base da ram
 	STRB R0,[R2,#1];;troca os valores de posicao
 	STRB R1,[R2];;troca os valores de posicao
@@ -84,6 +116,6 @@ DONE
 	NOP
 
 	
-TEXTO DCB 1,2,3,4,0
+TEXTO DCB 3,10,73,127,43,14,211,3,203,5,21,7,206,233,157,237,241,105,252,19,0;; O 0 indicca o fim da lista
 	ALIGN                           ; garante que o fim da seção está alinhada 
     END                             ; fim do arquivo
